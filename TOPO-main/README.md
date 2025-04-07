922aab2334523ee80721b400a8cfa87376fc0070
aVr0Ra <ysmormichael@gmail.com>
2025-04-07 17:32:22 +0800

    First commit

---
+++ TOPO-main/README.md
@@ -0,0 +1,78 @@
+# Optimizing NOTEARS Objectives via Topological Swaps <img src="https://github.com/Duntrain/TOPO/assets/6846921/fa8812bc-b295-4200-9ce0-4c267aafc9ce" width="5%" height="5%">
+
+This is an implementation of the following paper:
+
+[1] C. Deng, K. Bello, B. Aragam, P. Ravikumar. (2023). [Optimizing NOTEARS Objectives via Topological Swaps][topo]. [ICML'23](https://icml.cc/Conferences/2023) 
+
+
+[notears]: https://arxiv.org/abs/1803.01422
+[dagma]: https://arxiv.org/abs/2209.08037
+[topo]: https://arxiv.org/abs/2305.17277
+
+If you find this code useful, please consider citing:
+
+```
+@inproceedings{deng2023optimizing,
+  title={Optimizing NOTEARS Objectives via Topological Swaps},
+  author={Chang, Deng and Kevin, Bello and Bryon, Aragam and Pradeep, Ravikumar},
+  booktitle={Proceedings of the 40th International Conference on Machine Learning},
+  year={2023}
+}
+```
+
+## Summary
+
+In this paper, we propose a bi-level algorithm that solve a class of non-convex optimization problems that has emerged in the context of learning directed acyclic graphs (DAGs). These problems (e.g. [NOTEARS][notears]) involve minimizing a given loss or score function, subject to a non-convex continuous constraint that penalizes the presence of cycles in a graph. Compared to the previous work, our algorithm: (1) Is guaranteed to find a local minimum or a KKT point under weaker conditions; (2) Achieves better score; (3) leads to siginificant improvement in structure recovery (e.g. SHD).
+
+## Requirements
+
+- Python 3.6+
+- `numpy`
+- `scipy`
+- `python-igraph`
+- `torch`: Only used for nonlinear 
+- `scikit-learn`
+
+
+## Contents 
+
+- `Topo_linear.py` - implementation of TOPO for linear models (also support Logistic losses).
+- `Topo_nonlinear.py` - implementation of TOPO for nonlinear models.
+- `Topo_utils` - implementation of support function for TOPO.
+- `utils.py` - graph simulation, data simulation, and accuracy evaluation.
+
+## Running TOPO
+
+Use `requirements.txt` to install the dependencies (recommended to use virtualenv or conda).
+The simplest way to try out TOPO is to run a simple (linear) example:
+```bash
+$ git clone https://github.com/Duntrain/TOPO.git
+$ cd TOPO/
+$ pip3 install -r requirements.txt
+$ python3 Topo_linear.py
+```
+
+or a simple (nonlinear) example. you can replace `PRINT_ENABLED = True` to `PRINT_ENABLED = False` in script to suppress the printing of intermediate results.
+```bash
+$ git clone https://github.com/Duntrain/TOPO.git
+$ cd TOPO/
+$ pip3 install -r requirements.txt
+$ python3 Topo_nonlinear.py
+```
+
+
+The above runs the TOPO on a randomly generated 20-node Erdos-Renyi graph with 1000 samples. 
+The output should look like the below:
+```
+{'fdr': 0.0, 'tpr': 1.0, 'fpr': 0.0, 'shd': 0, 'nnz': 20}
+```
+## Extension
+
+Users can take advantage of the framework of TOPO to test different structure equation model (SEM) by simply writing their own regression and score function (i.e. `regress(X,y)`, `score(X,W)`) in `Topo_linear.py`. We have implemented those functions for Linear and Logistic model.
+
+## Acknowledgments
+
+We thank the authors of the [NOTEARS repo][notears-repo] for making their code available. Part of our code is based on their implementation, specially the `utils.py` file and some code from their implementation of nonlinear models.
+
+[notears-repo]: https://github.com/xunzheng/
+
---
+++ TOPO-main/all_brain_connectivity.py
@@ -0,0 +1,114 @@
+from scipy.io import loadmat
+import numpy as np
+from topo_linear import TOPO_linear, threshold_W
+from sklearn.linear_model import LinearRegression
+import utils
+from timeit import default_timer as timer
+from scipy.signal import butter, filtfilt
+import matplotlib.pyplot as plt
+from castle import MetricsDAG
+
+
+def bandpass_filter(data, lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    y = filtfilt(b, a, data, axis=0)
+    return y
+
+
+if __name__ == '__main__':
+    # 数据加载和预处理
+
+    for dataset in range(1,29):
+        data = loadmat(f'../datasets/sims/sim{dataset}.mat')
+        print('\nCurrent dataset: #', dataset)
+
+        net = data['net']  # shape: (Nsubjects, Nnodes, Nnodes)
+        ts = data['ts']  # shape: (Ntimepoints, Nnodes)
+        Nnodes = int(data['Nnodes'][0][0])
+        Nsubjects = int(data['Nsubjects'][0][0])
+        Ntimepoints = int(data['Ntimepoints'][0][0])
+
+        print("Original ts shape:", ts.shape)
+        # 如果需要滤波，可以取消下面两行的注释
+        # fs = 1 / 3.0  # TR=3s
+        # ts = bandpass_filter(ts, 0.01, 0.1, fs, order=3)
+
+        # 重塑 ts，使得每个受试者的数据都包含在内
+        ts_all = ts.reshape(Nsubjects, Ntimepoints, Nnodes)
+        print("Reshaped ts_all shape (subjects, timepoints, nodes):", ts_all.shape)
+
+        # 如果想要将所有受试者的数据拼接起来进行整体分析：
+        X_combined = ts_all.reshape(-1, Nnodes)
+        print("Combined X shape:", X_combined.shape)
+
+        # 结合所有受试者的网络，取平均后二值化作为真值
+        B_true_linear = (np.mean(net, axis=0) != 0)
+        print("B_true_linear shape:", B_true_linear.shape)
+
+        # 设置随机种子
+        rd_int = int(np.random.randint(10000, size=1)[0])
+        print(f"random seed: {rd_int}")
+        utils.set_random_seed(rd_int)
+
+        n, d = X_combined.shape
+        verbose = False
+
+
+        # 线性模型部分
+        def regress(X, y):
+            reg = LinearRegression(fit_intercept=False)
+            reg.fit(X=X, y=y)
+            return reg.coef_
+
+
+        def score(X, W):
+            M = X @ W
+            R = X - M
+            loss = 0.5 / X.shape[0] * np.sum(R ** 2)
+            G_loss = -1.0 / X.shape[0] * (X.T @ R)
+            return loss, G_loss
+
+
+        model_linear = TOPO_linear(regress=regress, score=score)
+        topo_init = list(np.random.permutation(range(d)))
+        start = timer()
+        W_linear, topo_linear_est, Z_linear, loss_linear = model_linear.fit(X=X_combined, topo=topo_init, verbose=verbose)
+        end = timer()
+        print(f"Linear model time: {end - start:.4f}s")
+
+        # 绘图调试（可选）
+        '''
+        plt.figure()
+        plt.imshow(W_linear, cmap='viridis', interpolation='none')
+        plt.colorbar()
+        plt.title("Estimated Connectivity Weights")
+        plt.xlabel("Target Node")
+        plt.ylabel("Source Node")
+        plt.show()
+        '''
+
+        B_est = (threshold_W(W=W_linear) != 0).astype(int)
+        B_true_linear = np.array(B_true_linear).astype(int)
+        # print("Estimated B (B_est):")
+        # print(B_est)
+        # print("True B (B_true_linear):")
+        # print(B_true_linear)
+        met = MetricsDAG(B_est, B_true_linear)
+        print("Metrics:", met.metrics)
+
+
+
+'''
+fdr (False Discovery Rate)：误报的比例
+
+tpr (True Positive Rate)：有大约76.19%被模型正确识别出来，也就是召回率
+
+fpr (False Positive Rate)：模型错误地预测为存在连接的比例约为29.17%
+
+shd (Structural Hamming Distance)：预测网络与真实网络之间需要修改的边数
+
+nnz (Non-zero count)：值为13，表示在阈值处理后，模型预测出总共有13条连接（即非零元素的个数）。
+'''
